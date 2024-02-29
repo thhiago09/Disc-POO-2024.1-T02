1c140150723ee77132c1d5ad3b8310849364847d
Jao <160160569+JaoTitow@users.noreply.github.com>
2024-02-28 22:07:05 -0300

    Add files via upload

---
+++ CAP-03/EXERCICIOS-RESOLVIDOS/QR01/src/main/Main.java
@@ -0,0 +1,32 @@
+package main;
+import java.util.Scanner;
+
+public class Main {
+	
+	 static  void calc(int n, int n2) {
+		 
+	 }
+	
+	
+	public static void main(String[] args) {
+		Scanner num1 = new Scanner(System.in);
+		int n;
+		System.out.printf("Informe um número: \n");
+		n = num1.nextInt();
+		
+		Scanner num2 = new Scanner(System.in);
+		int n2;
+		System.out.printf("Informe um número: \n");
+		n2 = num2.nextInt();
+		
+			calc(n, n2);
+			
+			System.out.println(n + n2);
+			System.out.println(n - n2);
+			System.out.println(n / n2);
+			System.out.println(n * n2);
+	
+	}
+			
+	
+}
